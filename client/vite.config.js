85883b64aa6f51b37d2ceb27a0b29945ce9677d4
Akhilesh Srivastav <srivastavakhilesh157@gmail.com>
2024-12-02 20:15:32 +0530

    push

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,12 +1,30 @@
-import { defineConfig } from "vite";
+import path from "path";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  server: {
-    proxy: {
-      "/api": "http://localhost:3000",
+export default defineConfig(({ mode }) => {
+  // Load environment variables based on the current mode
+  const env = loadEnv(mode, process.cwd(), "");
+
+  return {
+    plugins: [react()],
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
+      },
     },
-  },
-  plugins: [react()],
+    server: {
+      proxy:
+        mode === "development"
+          ? {
+              "/api": {
+                target: env.VITE_API_BASE_URL,
+                changeOrigin: true,
+                rewrite: (path) => path.replace(/^\/api/, ""),
+              },
+            }
+          : undefined,
+    },
+  };
 });
--- server/index.js
+++ server/index.js
@@ -2,10 +2,20 @@ import express from "express";
 import router from "./router/api.js";
 import connectToDB from "./connect.js";
 import dotenv from "dotenv";
+import cors from "cors";
 dotenv.config();
 
 const PORT = 3000;
 const app = express();
+
+// congifure cors
+app.use(
+  cors({
+    origin: "", // Allow requests from any origin
+    methods: ["GET", "POST", "PUT", "DELETE"], // Specify allowed HTTP methods
+    allowedHeaders: ["Content-Type", "Authorization"], // Specify allowed headers
+  })
+);
 
 // api level middleware.
 app.use(express.json());
